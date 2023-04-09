01cdce19d954c6d7664e7d424c834888f3401515
edmerix <teddymerricks@hotmail.com>
2023-04-09 17:36:44 -0400

    Old API no longer active, shifted to MLB API, complete overhaul

--- src/mlb.php
+++ src/mlb.php
@@ -1,5 +1,4 @@
 <?php
-//TODO: I started changing to inline variable names in strings rather than "example ".$variable." string" - need to finish
 date_default_timezone_set("America/New_York");
 
 require_once('workflows.php');
@@ -18,8 +17,6 @@ if($hour < 3){ // if it's before 3 a.m. we're probably asking for yesterday's ga
 	$year = date("Y");
 }
 
-$teamcodes = ['ARI', 'ATL', 'BAL', 'BOS', 'CHC', 'CWS', 'CIN', 'CLE', 'COL', 'DET', 'FLA', 'HOU', 'KAN', 'LAA', 'LAD', 'MIL', 'MIN', 'NYM', 'NYY', 'OAK', 'PHI', 'PIT', 'SD', 'SF', 'SEA', 'STL', 'TB', 'TEX', 'TOR', 'WAS'];
-
 $team = urlencode($argv[1]);
 $team = strtoupper($team);
 if(strlen($team) < 2){
@@ -28,115 +25,237 @@ if(strlen($team) < 2){
 	exit;
 }
 
-if(!in_array($team, $teamcodes)){
-	$w->result(0, 'na', "Team $team not found", "", $icon, "no");
-}else{
+$url = "http://statsapi.mlb.com/api/v1/teams?sportID=1&".time();
 
-	$url = "http://gd2.mlb.com/components/game/mlb/year_$year/month_$month/day_".sprintf('%02d',$day)."/master_scoreboard.json?now=".date("dmyhms");
+$data = $w->request($url);
+$data = json_decode($data);
 
-	$data = $w->request($url);
-	$data = json_decode($data);
+$teamCodes = array_column($data->teams, 'abbreviation');
+$keys = array_keys(preg_grep("/$team/i", $teamCodes));
 
-	$data = $data->data->games;
+if(!$keys || count($keys) < 1){
+	$w->result(0, 'na', "No match for $team", "Try again", $icon, "no");
+	echo $w->toxml();
+	return;
+}
+$teamID = -1;
+$teamName = '';
+$teamData = -1;
+$d = 0;
+for($k = 0; $k < count($keys); $k++){
+	if(strcasecmp($data->teams[$keys[$k]]->abbreviation,$team) == 0){
+		$teamData = $data->teams[$keys[$k]];
+		$teamName = $teamData->teamName;
+		$teamID = $teamData->id;
+		$teamAbbr = $teamData->abbreviation;
+	}
+}
+if($teamID < 0){
+	$w->result(0, 'na', "No match for $team", "Try again", $icon, "no");
+	echo $w->toxml();
+	return;
+}
 
-	$awayTeams = array_column($data->game, 'away_name_abbrev');
-	$homeTeams = array_column($data->game, 'home_name_abbrev');
-	
-	$homeaway = '';
-	$n = -1;
-	if(in_array($team,$awayTeams)){
-		$homeaway = 'away';
-		$n = array_keys($awayTeams, $team);
-	}elseif(in_array($team,$homeTeams)){
+$url = "http://statsapi.mlb.com/api/v1/schedule/games/?sportId=1&date=$year-$month-".sprintf('%02d',$day)."&".time();
+
+$data = $w->request($url);
+$data = json_decode($data);
+
+$games = $data->dates[0]->games;
+
+$awayGames = [];
+$homeGames = [];
+
+
+for($g = 0; $g < count($games); $g++){
+	if(strcasecmp($games[$g]->teams->away->team->id,$teamID) == 0){
+		$awayGames[] = $g;
+	}elseif(strcasecmp($games[$g]->teams->home->team->id,$teamID) == 0){
+		$homeGames[] = $g;
+	}
+}
+
+$allGames = $awayGames + $homeGames;
+if(count($allGames) < 0){
+	$w->result(0, 'na', "No $teamName game found for today", "", $icon, "no");
+	return;
+}
+
+$d = 0;
+for($n = 0; $n < count($allGames); $n++){
+	$status = $games[$allGames[$n]]->status->statusCode;
+	if(in_array($allGames[$n],$homeGames)){
 		$homeaway = 'home';
-		$n = array_keys($homeTeams, $team);
+	}elseif(in_array($allGames[$n],$awayGames)){
+		$homeaway = 'away';
+	}else{
+		$homeaway = 'unknown';
+	}
+	$homeawayInv = array('home' => 'away', 'away' => 'home'); // easy way to search the opposite
+
+	// Information about teams (e.g. team name rather than full name)
+	$homeTeamLink = $games[$allGames[$n]]->teams->home->team->link;
+	$awayTeamLink = $games[$allGames[$n]]->teams->away->team->link;
+	$homeTeamURL = "http://statsapi.mlb.com/$homeTeamLink";
+	$homeTeamData = $w->request($homeTeamURL);
+	$homeTeamData = json_decode($homeTeamData);
+	$awayTeamURL = "http://statsapi.mlb.com/$awayTeamLink";
+	$awayTeamData = $w->request($awayTeamURL);
+	$awayTeamData = json_decode($awayTeamData);
+	
+	$homeAbbr = $homeTeamData->teams[0]->teamName;
+	$awayAbbr = $awayTeamData->teams[0]->teamName;
+	
+	$winPct = $games[$allGames[$n]]->teams->$homeaway->leagueRecord->pct;
+	$winRecord = $games[$allGames[$n]]->teams->$homeaway->leagueRecord->wins;
+	$lossRecord = $games[$allGames[$n]]->teams->$homeaway->leagueRecord->losses;
+
+	$swap = $homeawayInv[$homeaway];
+	$opponentFullName = $games[$allGames[$n]]->teams->$swap->team->name;
+	$opponentWinPct = $games[$allGames[$n]]->teams->$swap->leagueRecord->pct;
+	$opponentWinRecord = $games[$allGames[$n]]->teams->$swap->leagueRecord->wins;
+	$opponentLossRecord = $games[$allGames[$n]]->teams->$swap->leagueRecord->losses;
+	
+	switch($homeaway){
+		case "away":
+			$opponentName = $homeAbbr;
+			break;
+		case "home":
+			$opponentName = $awayAbbr;
+			break;
+		default:
+			$opponentName = $opponentFullName;
 	}
 	
-	$tot_n = count($n);
+	$location = $games[$allGames[$n]]->venue->name;
+	$gameInSeries = $games[$allGames[$n]]->seriesGameNumber;
+	$totalInSeries = $games[$allGames[$n]]->gamesInSeries;
+	
+	$contentLink = $games[$allGames[$n]]->content->link;
+	$contentURL = "http://statsapi.mlb.com/$contentLink";
+	$contentData = $w->request($contentURL);
+	$contentData = json_decode($contentData);
 
-	if($homeaway == "" || $tot_n < 0){
-		$w->result(0, 'na', "No $team game found for today", "", $icon, "no");
-	}else{
-		$d = 0;
-		for($gn = 0; $gn < $tot_n; $gn++){
-			$game = $data->game[$n[$gn]];
-			if(strcasecmp($game->status->status, "In Progress") == 0){
-				//echo $game->away_team_name.":". $game->linescore->r->away." | ".$game->home_team_name.": ".$game->linescore->r->home;
-				$info = $game->away_team_name." ". $game->linescore->r->away." - ".$game->linescore->r->home." ".$game->home_team_name;
-				$info .= " (".$game->status->inning_state." of the ".$game->status->inning;
-				switch($game->status->inning){
-					case 1:
-					$info .= "st";
-					break;
-					case 2:
-					$info .= "nd";
-					break;
-					case 3:
-					$info .= "rd";
-					break;
-					default:
-					$info .= "th";
-				}
-				$info .= ")";
-				$w->result($d++, "na", $info, "Pitcher: ".$game->pitcher->last." (".$game->pitcher->era.") | Batter: ".$game->batter->last. " (".$game->batter->avg.")", $icon, "no");
-				// linescore (r, h & e - both teams)
-				$w->result($d++, "na", "R: ".$game->linescore->r->home." - ".$game->linescore->r->away." | H: ".$game->linescore->h->home." - ".$game->linescore->h->away." | E:".$game->linescore->e->home." - ".$game->linescore->e->away, "$game->home_team_name | ".$game->away_team_name, $icon, "no");
-				// b, s, o
-				if(strcasecmp($game->status->inning_state, "top") == 0){
-					$inning_info = "$game->home_team_name fielding (".$game->pitcher->last." pitching)";
-				}elseif(strcasecmp($game->status->inning_state, "bottom") == 0){
-					$inning_info = "$game->home_team_name batting (".$game->batter->last." at bat)";
-				}else{
-					$inning_info = $game->status->inning_state." of inning";
-				}
-				$w->result($d++, "na", "Balls: ".$game->status->b." | Strikes: ".$game->status->s." | Outs: ".$game->status->o, $inning_info, $icon, "no");
-				// runners_on_base
-				$bases = array("-","-","-");
-				$base_icon = "000";
-				$b = 0;
-				if(property_exists($game->runners_on_base,'runner_on_1b')){
-					$bases[0] = "1st: ".$game->runners_on_base->runner_on_1b->last;
-					$base_icon[0] = "1";
-					$b++;
-				}
-				if(property_exists($game->runners_on_base,'runner_on_2b')){
-					$bases[1] = "2nd: ".$game->runners_on_base->runner_on_2b->last;
-					$base_icon[1] = "1";
-					$b++;
-				}
-				if(property_exists($game->runners_on_base,'runner_on_3b')){
-					$bases[2] = "3rd: ".$game->runners_on_base->runner_on_3b->last;
-					$base_icon[2] = "1";
-					$b++;
-				}
-				$base_sub = $b." runner";
-				if($base_sub != 1) $base_sub .= "s";
-				$base_sub .= " on base";
-				if($base_sub != 1) $base_sub .= "s";
-				$w->result($d++, "na", implode($bases," | "), $base_sub, $base_icon.".png", "no");
-				// away_win, away_loss, home_win, home_loss
-				$w->result($d++, "na", "$game->home_team_name: ".$game->home_win."-".$game->home_loss, $game->away_team_name.": ".$game->away_win."-".$game->away_loss, $icon, "no");
-				$w->result($d++, "na", "TV: ".$game->broadcast->$homeaway->tv, "Radio: ".$game->broadcast->$homeaway->radio, $icon, "no");
-			}elseif(strcasecmp($game->status->status, "Preview") == 0 || strcasecmp($game->status->status, "Pre-Game") == 0){
-				if($homeaway == "home"){
-					$gametime = $game->home_time;
-					$ampm = $game->home_ampm;
-				}else{
-					$gametime = $game->away_time;
-					$ampm = $game->away_ampm;
-				}
-				$w->result($d++, "na", $game->away_name_abbrev." @ ".$game->home_name_abbrev." at ".$gametime." ".$ampm." (searched team time zone)", $game->venue, $icon, "no");
-				$w->result($d++, "na", "$game->home_team_name: ".$game->home_win."-".$game->home_loss, $game->away_team_name.": ".$game->away_win."-".$game->away_loss, $icon, "no");
-				$w->result($d++, "na", "TV: ".$game->broadcast->$homeaway->tv, "Radio: ".$game->broadcast->$homeaway->radio, $icon, "no");
-			}elseif(strcasecmp($game->status->status, "Final") == 0 || strcasecmp($game->status->status, "Game Over") == 0){
-				$w->result($d++, "na", "Final: $game->home_team_name ".$game->linescore->r->home." - ".$game->linescore->r->away." ".$game->away_team_name, "R: ".$game->linescore->r->home." - ".$game->linescore->r->away." | H: ".$game->linescore->h->home." - ".$game->linescore->h->away." | E: ".$game->linescore->e->home." - ".$game->linescore->e->away."", $icon, "no");
-			}elseif(strcasecmp($game->status->status, "Warmup") == 0){
-				$w->result($d++, "na", "Warmup", "Gettin' ready...", $icon, "no");
+	$broadcasters = array_column($contentData->media->epg[0]->items, 'callLetters');
+	$broadcastList = implode("; ", $broadcasters);
+	
+	$gameLink = $games[$allGames[$n]]->link;
+	$gameURL = "http://statsapi.mlb.com/$gameLink";
+	$gameData = $w->request($gameURL);
+	$gameData = json_decode($gameData);
+	
+	switch($status){
+		case "F":  //Final
+		case "O":  //Game Over
+			if($games[$allGames[$n]]->teams->$homeaway->isWinner){
+				$winlose = ": win!";
 			}else{
-				$w->result($d++, "na", "Unknown game state", "Can't find details for game status: ".$game->status->status, $icon, "no");
+				$winlose = " :( ";
 			}
-		}
+			$primaryScore = $games[$allGames[$n]]->teams->$homeaway->score;
+			$opponentScore = $games[$allGames[$n]]->teams->$swap->score;
+
+			$w->result($d++, "na", "Final$winlose $teamName $primaryScore – $opponentScore $opponentName", "$teamName: $winPct ($winRecord - $lossRecord) | $opponentName: $opponentWinPct ($opponentWinRecord - $opponentLossRecord)", $icon, "no");
+			break;
+		case "S":  //Scheduled
+		case "P":  //Pre-game
+		case "PW": //Pre-game, warmup
+			$scheduledTime = strtotime($games[$allGames[$n]]->gameDate);
+			$startTime = date('H:i', $scheduledTime);
+			
+			$ourPitcher = $gameData->gameData->probablePitchers->$homeaway->fullName;
+			$theirPitcher = $gameData->gameData->probablePitchers->$swap->fullName;
+			
+			$w->result($d++, "na", "Against $opponentName ($homeaway game) @ $startTime", "At $location", $icon, "no");
+			$w->result($d++, "na", "Game $gameInSeries in $totalInSeries game series", "$teamName: $winPct ($winRecord - $lossRecord) | $opponentName: $opponentWinPct ($opponentWinRecord - $opponentLossRecord)", $icon, "no");
+			$w->result($d++, "na", "Likely starting pitcher: $ourPitcher", "$opponentName likely starting pitcher: $theirPitcher", $icon, "no");
+			$w->result($d++, "na", "Broadcast on: $broadcastList", "", $icon, "no");
+			break;
+		case "I":  //In-game
+			$game = $games[$allGames[$n]]; // necessary?
+			$gameID = $game->gamePk; // necessary?
+			
+			$linescore = $gameData->liveData->linescore;
+			$inning = $linescore->currentInningOrdinal;
+			$inningState = $linescore->inningState;
+			$balls = $linescore->balls;
+			$strikes = $linescore->strikes;
+			$outs = $linescore->outs;
+			
+			$runners = $gameData->liveData->plays->currentPlay->runners;
+			
+			$pitcher = $linescore->defense->pitcher->fullName;
+			$atBat = $linescore->offense->batter->fullName;
+			$battingOrder = $linescore->offense->battingOrder;
+			$inHole = $linescore->offense->inHole->fullName;
+			$onDeck = $linescore->offense->onDeck->fullName;
+			
+			$homeNoHitter = $gameData->gameData->flags->homeTeamNoHitter;
+			$homePerfectGame = $gameData->gameData->flags->homeTeamPerfectGame;
+			$awayNoHitter = $gameData->gameData->flags->awayTeamNoHitter;
+			$awayPerfectGame = $gameData->gameData->flags->awayTeamPerfectGame;
+			
+			$runs = array('home' => $linescore->teams->home->runs, 'away' => $linescore->teams->away->runs);
+			$hits = array('home' => $linescore->teams->home->hits, 'away' => $linescore->teams->away->hits);
+			$errors = array('home' => $linescore->teams->home->errors, 'away' => $linescore->teams->away->errors);
+			$leftOnBase = array('home' => $linescore->teams->home->leftOnBase, 'away' => $linescore->teams->away->leftOnBase);
+			
+			$awayName = $games[$allGames[$n]]->teams->away->team->name;
+			$homeName = $games[$allGames[$n]]->teams->home->team->name;
+			
+			$topLine = "$awayAbbr {$runs['away']} – {$runs['home']} $homeAbbr ($inningState of the $inning)";
+			$subLine = "At bat: $battingOrder. $atBat ($onDeck on deck; $inHole in the hole)";
+			$w->result($d++, "na", $topLine, $subLine, $icon, "no");
+			
+			$topLine = "R: {$runs['away']} - {$runs['home']} | H: {$hits['away']} - {$hits['home']} | E: {$errors['away']} - {$errors['home']}";
+			$subLine = "LOB: {$leftOnBase['away']} | {$leftOnBase['home']} ($awayAbbr | $homeAbbr)";
+			$w->result($d++, "na", $topLine, $subLine, $icon, "no");
+			
+			$topLine = "Balls: $balls | Strikes: $strikes | Outs: $outs";
+			$subLine = "Pitcher: $pitcher";
+			$w->result($d++, "na", $topLine, $subLine, $icon, "no");
+			
+			// runners_on_base
+			$bases = array("-","-","-");
+			$base_icon = "000";
+			for($r = 0; $r < count($runners); $r++){
+				if(!$runners[$r]->movement->isOut){
+					$where = $runners[$r]->movement->end;
+					switch($where){
+						case "1B":
+						$bases[0] = "1st: {$runners[$r]->details->runner->fullName}";
+						$base_icon[0] = "1";
+						break;
+						case "2B":
+						$bases[1] = "2nd: {$runners[$r]->details->runner->fullName}";
+						$base_icon[1] = "1";
+						break;
+						case "3B":
+						$bases[2] = "3rd: {$runners[$r]->details->runner->fullName}";
+						$base_icon[2] = "1";
+						break;
+					}
+				}				
+			}
+			
+			$b = count($runners);
+			$base_sub = $b." runner";
+			if($b != 1) $base_sub .= "s";
+			$base_sub .= " on base";
+			if($base_sub != 1) $base_sub .= "s";
+			$w->result($d++, "na", implode(" | ",$bases), $base_sub, $base_icon.".png", "no");
+			
+			$topLine = "Yankees: $winPct ($winRecord - $lossRecord) | $opponentName: $opponentWinPct ($opponentWinRecord - $opponentLossRecord)";
+			$subLine = "Game $gameInSeries in $totalInSeries game series";
+			$w->result($d++, "na", $topLine, $subLine, $icon, "no");
+			
+			$w->result($d++, "na", "Broadcast on: $broadcastList", "", $icon, "no");
+			
+			break;
+		default:
+			$w->result($d++, "na", "Unknown game state", "Can't find details for game status: $status (\"".$games[$allGames[$n]]->status->detailedState."\")", $icon, "no");
 	}
+	
 }
 
 echo $w->toxml();
